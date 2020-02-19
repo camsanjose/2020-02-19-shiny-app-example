001ca7f5769bc88555dbdcd62cb7a860ca5d72f7
Camila <100419816@alumnos.uc3m.es>
2020-02-19 12:18:52 +0100

    choices entered via updateSelectInput

--- app.R
+++ app.R
@@ -12,8 +12,6 @@ library(ggplot2)
 library(tidyverse)
 
 
-list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
-names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -21,21 +19,35 @@ ui <- fluidPage(
     titlePanel("This is a new Shiny app"),
     includeMarkdown("references.md"),
     selectInput("select", label = h3("Plot by type of alimentation"), 
-                choices = list_choices,
+                choices = character(0),
                 selected = 1),
     h3("Plots"),
     plotOutput(outputId = "plot")
 
     )
 col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
+list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
+names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+  # Can also set the label and select items
+  updateSelectInput(session, "select",
+                    choices = list_choices,
+                    selected = tail(list_choices, 1)
+  )
+  
   output$plot<-renderPlot({
+    # cat(file=stderr(), "input$select:", input$select == "", "\n")
+  if(input$select != ""){
     ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
-      scale_x_log10() +  col_scale + geom_point()
+      scale_x_log10() +
+      col_scale +
+      geom_point()
+    }
   })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
