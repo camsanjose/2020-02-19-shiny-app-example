d8028ad295fed4a5439267f931455e0b5a7351e6
Camila <100419816@alumnos.uc3m.es>
2020-02-19 12:12:17 +0100

    add selection of mammals by "vore" variable

--- app.R
+++ app.R
@@ -10,23 +10,30 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
+
+
+list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
+names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
     titlePanel("This is a new Shiny app"),
     includeMarkdown("references.md"),
+    selectInput("select", label = h3("Plot by type of alimentation"), 
+                choices = list_choices,
+                selected = 1),
     h3("Plots"),
     plotOutput(outputId = "plot")
 
     )
+col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   output$plot<-renderPlot({
-    ggplot(msleep, aes(bodywt, sleep_total, colour = vore)) +
-      scale_x_log10() +
-      geom_point() + facet_wrap(~ vore, nrow = 2)
+    ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
+      scale_x_log10() +  col_scale + geom_point()
   })
 }
 
